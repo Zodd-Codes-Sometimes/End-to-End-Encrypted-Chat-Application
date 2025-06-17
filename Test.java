b66c8f68ae022700d0bcd2cb68e2fc1b3d123cfd
Zodd <149360941+Zodd-Codes-Sometimes@users.noreply.github.com>
2025-06-18 04:39:12 +0500

    .

--- Test.java
+++
@@ -1,6 +0,0 @@
-// This is a simple Java program to demonstrate a basic structure
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Testing the repo");
-    }
-}
