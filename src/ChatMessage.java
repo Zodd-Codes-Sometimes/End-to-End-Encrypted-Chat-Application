788f22f5b14c763f248001b278dcf416bafa9c02
ahsanazam101 <ahsanazam930@gmail.com>
2025-06-17 23:37:55 +0500

    Update ChatMessage.java

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,1 +1,19 @@
-todo
+import java.io.Serializable;
+
+public class ChatMessage implements Serializable {
+    private final String sender;
+    private final String message;
+
+    public ChatMessage(String sender, String message) {
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
