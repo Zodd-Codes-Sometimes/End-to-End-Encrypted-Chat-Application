c168095bd1284cca8c33e8b6a5ebd9e236e7fc8f
four0four <88083718+f0ur0four@users.noreply.github.com>
2025-06-19 15:01:51 +0500

    Updated RSAUtils and Uploaded SerializationUtils

--- src/utils/RSAUtils.java
+++ src/utils/RSAUtils.java
@@ -3,7 +3,11 @@ package utils;
 import javax.crypto.Cipher;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class RSAUtils {
     // Function to generate public/private key pair for the user, the key size indicates the number of bits of the key
@@ -18,8 +22,26 @@ public class RSAUtils {
     public static String encrypt(String data, PublicKey publicKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encBytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
-        String enc = Base64.getEncoder().encodeToString(encBytes);
+
+        byte[] inBytes = data.getBytes(StandardCharsets.UTF_8);
+        int keySizeBytes = ((RSAPublicKey) publicKey).getModulus().bitLength() / 8;
+        // RSA uses PKCS#1 v1.5 padding which takes 11 bytes so the maximum chunk size will be => key size (in bytes) - 11
+        int maxChunkSize = keySizeBytes - 11;
+        /*
+        The number of bytes that RSA can encrypt depends on the key size. A key of 2048 bits / 256 bytes will encrypt maximum 245 bytes (11 bytes are for the padding)
+        Since the data to encrypt can be longer than 245 bytes, we break down the encryption into chunks and then encrypt the chunks separately
+         */
+        List<byte[]> encChunks = new ArrayList<>();
+
+        for(int i = 0; i <= inBytes.length; i += maxChunkSize){
+            int len = Math.min(maxChunkSize, inBytes.length - i);
+            byte[] chunk = new byte[len];
+            System.arraycopy(inBytes, i, chunk, 0, len);
+            encChunks.add(cipher.doFinal(chunk));
+        }
+
+        byte[] encryptedBytes = joinChunks(encChunks);
+        String enc = Base64.getEncoder().encodeToString(encryptedBytes);
         return enc;
     }
 
@@ -27,9 +49,35 @@ public class RSAUtils {
     public static String decrypt(String data, PrivateKey privateKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] enc = Base64.getDecoder().decode(data);
-        byte[] decBytes = cipher.doFinal(enc);
+        byte[] encBytes = Base64.getDecoder().decode(data);
+        int keySizeBytes = ((RSAPrivateKey) privateKey).getModulus().bitLength() / 8;
+        // Decrypt the data in chunks then join the chunks
+        List<byte[]> decChunks = new ArrayList<>();
+
+        for(int i = 0; i < encBytes.length; i += keySizeBytes){
+            int len = Math.min(keySizeBytes, encBytes.length - i);
+            byte[] chunk = new byte[len];
+            System.arraycopy(encBytes, i, chunk, 0, len);
+            decChunks.add(cipher.doFinal(chunk));
+        }
+
+        byte[] decBytes = joinChunks(decChunks);
         String dec = new String(decBytes, StandardCharsets.UTF_8);
         return dec;
+    }
+
+    // Helper Function to join the chunks
+    public static byte[] joinChunks(List<byte[]> chunks){
+        int length = 0;
+        for(byte[] chunk: chunks){
+            length += chunk.length;
+        }
+        byte[] joinedChunks = new byte[length];
+        int currentPosition = 0;
+        for(byte[] chunk: chunks) {
+            System.arraycopy(chunk, 0, joinedChunks, currentPosition, chunk.length);
+            currentPosition += chunk.length;
+        }
+        return joinedChunks;
     }
 }
